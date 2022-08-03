14e82221562066142a8de945f28998b8024b6fd4
fesiddi <sfdev25@gmail.com>
2022-08-03 18:39:34 +0200

    fix(api) fix github action test_db_env and fix return if 0 articles found

--- routes/api/api.js
+++ routes/api/api.js
@@ -37,6 +37,11 @@ router.get('/articles/', async (req, res, next) => {
     }
     try {
         const result = await getArticles(siteName, keyword);
+        if (result.length === 0) {
+            return res.status(200).json({
+                NoData: `Sorry, no articles with ${keyword} keyword were found`,
+            });
+        }
         return res.status(200).json(result);
     } catch (err) {
         // passing error the to errorHandler middleware
