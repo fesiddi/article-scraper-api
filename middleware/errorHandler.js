58ea4bf1e16e358cb05ea25eedf437425496c52d
fesiddi <sfdev25@gmail.com>
2022-08-31 18:53:37 +0200

    fix(api) remove duplicate error case from errorHandler

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -4,12 +4,6 @@ const errorHandler = (error, req, res, next) => {
         return res.status(409).json({
             Error: 'Conflict, url is already present in the DB',
         });
-        // this part check for error of url format of the req.body.url
-    } else if (error.name === 'urlError') {
-        return res.status(400).json({
-            Error: error.message,
-        });
-        // this part check for error if website does not exist
     } else if (error.name === 'timeoutError') {
         return res.status(404).json({
             Error: error.message,
