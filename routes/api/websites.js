6404f853e385db301354d7571802f376d5e37cf9
fesiddi <sfdev25@gmail.com>
2022-09-02 18:21:01 +0200

    fix(api) delete unused import

--- routes/api/websites.js
+++ routes/api/websites.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const ROLES_LIST = require('../../utils/roles_list');
 const verifyRole = require('../../middleware/verifyRole');
-const verifyJWT = require('../../middleware/verifyJWT');
 const postWebsite = require('../../controllers/websites/postWebsite');
 const { getWebsites } = require('../../controllers/websites/getWebsites');
 const deleteWebsite = require('../../controllers/websites/deleteWebsite');
